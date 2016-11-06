c9fb0fe11f4074977c7a81016f646ca331c3a632
atomnook <atomnook.github@gmail.com>
2016-11-06 19:29:27 +0900

    Add multiple attack skill page

--- tool/app/controllers/SkillController.scala
+++ tool/app/controllers/SkillController.scala
@@ -3,7 +3,8 @@ package controllers
 import javax.inject.Inject
 
 import domain.service.DatabaseService
-import models.{ModelHelper, MultipleAttackFormat}
+import model.skill.MultipleAttackSkill
+import models.{Identifier, ModelHelper, MultipleAttackFormat}
 import play.api.mvc.{Action, Controller}
 
 class SkillController @Inject() (service: DatabaseService) extends Controller with JsonApiController {
@@ -16,10 +17,23 @@ class SkillController @Inject() (service: DatabaseService) extends Controller wi
     Ok(views.html.SkillController.list(skills))
   }
 
+  def redirect(id: String) = Action { request =>
+    if (service.multipleAttackSkills().read().exists(_.getSkill.getSkill.getId.id == id)) {
+      Redirect(routes.SkillController.getMultipleAttack(id))
+    } else {
+      throw new RuntimeException(s"$id not found")
+    }
+  }
+
   val multipleAttackList = Action { request =>
     val skills = service.multipleAttackSkills().read()
     val prerequisites = all
     Ok(views.html.SkillController.multipleAttackList(skills, prerequisites))
+  }
+
+  def getMultipleAttack(id: String) = Action { request =>
+    val skill = service.multipleAttackSkills().read().find(_.getSkill.getSkill.getId.id == id)
+    Ok(views.html.SkillController.getMultipleAttack(id, skill))
   }
 
   val createMultipleAttack = json[MultipleAttackFormat] { request =>
@@ -27,6 +41,12 @@ class SkillController @Inject() (service: DatabaseService) extends Controller wi
     val res = service.multipleAttackSkills().create(skill)
 
     if (res) created() else alreadyExists(request.body.id)
+  }
+
+  def deleteMultipleAttack(id: String) = Action { request =>
+    val res = service.multipleAttackSkills().delete(MultipleAttackSkill().update(_.skill.skill.id.id := id))
+
+    if (res) ok() else notFound(Identifier(id))
   }
 
   val chainAttackList = TODO
--- tool/app/models/BaseAndIncreasing.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-import play.api.libs.json.Json
-
-case class BaseAndIncreasing(base: Long, increasing: Long)
-
-object BaseAndIncreasing {
-  implicit val format = Json.format[BaseAndIncreasing]
-}
